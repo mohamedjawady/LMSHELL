2ebe664a3f3899f224949f125d27637fccc3ba93
mohamedjawady <mohamedhabib.jaouadi@etudiant-isi.utm.tn>
2023-01-19 18:38:22 +0100

    fixed typo + clean obj files

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 # install build tools for compilation
 
-sudo apt install build-tools -y
+sudo apt install build-essential -y
 
 # build executable
 make
@@ -8,3 +8,6 @@ make
 # add lmshell to bin path
 
 sudo cp lmshell /usr/bin/lmshell
+
+# clean generated files
+make clean
